948c0f3ae48d696b4399b85b6f03fbeba24e7cfe
softdevstory <developer@softdevstory.net>
2016-04-30 19:14:08 +0900

    SKTUtils source codes are added.

---
+++ Target/SKTUtilsExtended.h
@@ -0,0 +1,19 @@
+//
+//  SKTUtilsExtended.h
+//  SKTUtilsExtended
+//
+//  Created by HS Song on 2016. 4. 30..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKTUtilsExtended.
+FOUNDATION_EXPORT double SKTUtilsExtendedVersionNumber;
+
+//! Project version string for SKTUtilsExtended.
+FOUNDATION_EXPORT const unsigned char SKTUtilsExtendedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKTUtilsExtended/PublicHeader.h>
+
+
